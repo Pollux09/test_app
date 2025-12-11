8bc1fd3d651ddf18b184909b7899206c3d876e5b
pollux <artemgimranov57@gmail.com>
2025-12-11 19:26:41 +0400

    fix splash screen logics

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -18,16 +18,61 @@ class _SplashScreenState extends State<SplashScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (widget.storage.hasSub()) {
         Navigator.of(context).pushReplacementNamed('/home');
-      } else {
-        Navigator.of(context).pushReplacementNamed('/paywall');
       }
     });
   }
 
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: CircularProgressIndicator(color: Colors.white)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Добро пожаловать!',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w800,
+                color: Colors.black87,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/paywall');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 52,
+                  vertical: 18,
+                ),
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                elevation: 8,
+                shadowColor: Colors.blueAccent.withOpacity(0.4),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.arrow_circle_right_outlined, size: 26),
+                  const SizedBox(width: 12),
+                  Text('Продолжить'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
